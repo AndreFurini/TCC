aa8ed7a7ec99b236f5c1b2d84f215558251d23f5
AndreFurini <andre.furini@hotmail.com>
2026-06-11 19:50:06 -0300

    final10

--- app/Http/Controllers/OrdemServicoController.php
+++ app/Http/Controllers/OrdemServicoController.php
@@ -10,11 +10,6 @@ use Illuminate\Support\Facades\Auth;
 
 class OrdemServicoController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     // -------------------------------------------------------
     // INDEX — listagem de OS (Coordenador)
     // -------------------------------------------------------
